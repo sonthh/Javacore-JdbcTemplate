7cb3591a66632db7df94a7a9a1062a4dbbcedf13
tranhuuhongson <tranhuuhongson@gmail.com>
2019-04-13 22:58:42 +0700

    java core jdbctemplate lần 1

--- src/com/sontran/jdbctemplate/query/IJdbcTemplate.java
+++ src/com/sontran/jdbctemplate/query/IJdbcTemplate.java
@@ -7,30 +7,30 @@ import com.sontran.jdbctemplate.mapper.RowMapper;
 
 
 public interface IJdbcTemplate {
-	//RETURN LIST OBJECT
+	// RETURN LIST OBJECT
 	<T> List<T> query(String sql, Object[] parameters, RowMapper<T> rowMapper);
 	<T> List<T> query(String sql, RowMapper<T> rowMapper);
 	<T> List<T> query(String sql, Object[] parameters, BeanPropertyRowMapper<T> rowMapper);
 	<T> List<T> query(String sql, BeanPropertyRowMapper<T> rowMapper);
 	
-	//FOR OBJECT DATATYPE
+	// FOR OBJECT DATATYPE
 	<T> T queryForObject(String sql, Object[] parameters, RowMapper<T> rowMapper);
 	<T> T queryForObject(String sql, RowMapper<T> rowMapper);
 	<T> T queryForObject(String sql, Object[] parameters, BeanPropertyRowMapper<T> rowMapper);
 	<T> T queryForObject(String sql, BeanPropertyRowMapper<T> rowMapper);
 	
-	//FOR PRIMITY DATATYPE 
+	// FOR PRIMITY DATATYPE 
 	<T> T queryForObject(String sql, Class<T> clazz);
 	<T> T queryForObject(String sql, Object[] parameters, Class<T> clazz);
 	
 	<T> List<T> queryForList(String sql, Class<T> clazz);
 	<T> List<T> queryForList(String sql, Object[] parameters, Class<T> clazz);
 	
-	//UPDATE
+	// UPDATE
 	boolean update(String sql, Object[] parameters);
 	boolean update(String sql);
 	
-	//INSERT
+	// INSERT
 	//Long insert(String sql, Object... parameters);
 	Long insert(String sql, Object[] parameters);
 	Long insert(String sql);
